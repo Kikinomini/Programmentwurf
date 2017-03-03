839e4342ed2d64c09cbf3f0e43a5157c2796c032
MissKunterbunt <sarah.willibald@web.de>
2017-03-03 16:10:03 +0100

    Factory für USA

---
+++ https/github.com/Kikinomini/Programmentwurf.git/Programmentwurf/src/de/dhbw/it2016/pe/AddressClasses/AddressUS.java
@@ -0,0 +1,35 @@
+package de.dhbw.it2016.pe.AddressClasses;
+
+public class AddressUS implements Address {
+	
+	
+	private String street;
+	private String streetNumber;
+	private String city;
+	private String zipCode;
+	private String state;
+	
+	public AddressUS(String streetNumber, String street, String city, String state, String zipCode) {
+		this.streetNumber = streetNumber;		
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+	}
+	
+	public String formatAddress() {
+		String newline = System.getProperty("line.separator");
+		StringBuilder builder = new StringBuilder();
+		builder.append(streetNumber);
+		builder.append(" ");
+		builder.append(street);
+		builder.append(newline);
+		builder.append(city);
+		builder.append(", ");
+		builder.append(state);
+		builder.append(" ");
+		builder.append(zipCode);
+		return builder.toString();
+
+	}
+}	
