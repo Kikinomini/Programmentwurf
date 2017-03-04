07eb2e7305edd73f9394e7092570f2d51c9686e3
MissKunterbunt <sarah.willibald@web.de>
2017-03-04 16:36:19 +0100

    GB --> UK

---
+++ https/github.com/Kikinomini/Programmentwurf.git/Programmentwurf/src/de/dhbw/it2016/pe/PhoneNumberClasses/PhoneNumberUK.java
@@ -0,0 +1,33 @@
+package de.dhbw.it2016.pe.PhoneNumberClasses;
+
+public class PhoneNumberUK implements PhoneNumber {
+	
+	private String areaCode;
+	private String subscriber;
+
+	public PhoneNumberUK(String areaCode, String subscriber) {
+		this.areaCode = areaCode;
+		this.subscriber = subscriber;
+	}
+	
+
+	@Override
+	public String formatNational() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(areaCode);
+		builder.append("-");
+		builder.append(subscriber);
+		return builder.toString();
+	}
+
+	@Override
+	public String formatInternational() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("+44-");
+		builder.append(areaCode.substring(1));
+		builder.append("-");
+		builder.append(subscriber);
+		return builder.toString();
+	}
+
+}
