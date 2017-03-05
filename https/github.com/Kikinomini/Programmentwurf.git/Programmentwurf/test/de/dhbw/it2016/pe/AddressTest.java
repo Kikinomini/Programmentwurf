f6ace64c5b3199fed43994411e82a32b6eb55a5e
MissKunterbunt <sarah.willibald@web.de>
2017-03-05 14:08:13 +0100

    AdressTests ;)

--- https/github.com/Kikinomini/Programmentwurf.git/Programmentwurf/test/de/dhbw/it2016/pe/AddressTest.java
+++ https/github.com/Kikinomini/Programmentwurf.git/Programmentwurf/test/de/dhbw/it2016/pe/AddressTest.java
@@ -9,7 +9,7 @@ import de.dhbw.it2016.pe.AddressClasses.*;
 public class AddressTest {
 
 	@Test
-	public void testFormat() {
+	public void testFormatDE() {
 		AddressDE address = new AddressDE("Finkenweg", "11", "Berlin", "12345");
 		String expected = "Finkenweg 11\r\n12345 Berlin\r\n"
 				+ "Germany";
@@ -19,8 +19,8 @@ public class AddressTest {
 	@Test
 	public void testFormatUK() 
 	{
-		AddressUK address = new AddressUK("St. Nicholas Street", "10", "Coventry", "CV1 1JD");
-		String expected = "10 St. NicholasStreet\r\n"
+		AddressUK address = new AddressUK( "10", "St. Nicholas Street", "Coventry", "CV1 1JD");
+		String expected = "10 St. Nicholas Street\r\n"
 				+ "Coventry\r\n"
 				+ "CV1 1JD\r\n" 
 				+ "United Kingdom";
@@ -30,10 +30,39 @@ public class AddressTest {
 	@Test
 	public void testFormatUS() 
 	{
-		AddressUS address = new AddressUS("Spiral Drive", "15", "Florence", "KY", "41042");
+		AddressUS address = new AddressUS( "15", "Spiral Drive", "Florence", "KY", "41042");
 		String expected = "15 Spiral Drive\r\n"
-				+ "Florence, KY 41042"
+				+ "Florence, KY 41042\r\n"
 				+ "United States";
+		assertEquals(expected, address.formatAddress());
+	}
+	
+	@Test
+	public void testFormatFR()
+	{
+		AddressFR address = new AddressFR("Rue Barbet de Jouy", "16", "Paris", "75007");
+		String expected = "16 RUE BARBET DE JOUY\r\n"
+				+ "75007 PARIS\r\n"
+				+ "FRANCE";
+		assertEquals(expected, address.formatAddress());
+	}
+	
+	@Test
+	public void testFormatFN()
+	{
+		AddressFN address = new AddressFN("Ämmänkatu", "5", "Suomussalmi", "89600");
+		String expected = "Ämmänkatu 5\r\n"
+				+ "89600 Suomussalmi\r\n"
+				+ "Finland";
+		assertEquals(expected, address.formatAddress());
+	}
+	
+	@Test
+	public void testFormatCA(){
+		AddressCA address = new AddressCA("1601", "West Broadway", "Vancouver", "British Columbia", "BC V6J 1W9");
+		String expected = "1601 West Broadway\r\n"
+				+ "Vancouver, British Columbia BC V6J 1W9\r\n"
+				+ "Canada";
 		assertEquals(expected, address.formatAddress());
 	}
 }
