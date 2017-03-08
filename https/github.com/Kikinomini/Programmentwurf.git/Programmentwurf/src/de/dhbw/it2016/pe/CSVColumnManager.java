5c39b92d9fed29dfe75ba1635aa7eb52ebb2462c
Iyion <djaros96@yahoo.de>
2017-03-08 18:39:05 +0100

    added spaces

--- https/github.com/Kikinomini/Programmentwurf.git/Programmentwurf/src/de/dhbw/it2016/pe/CSVColumnManager.java
+++ https/github.com/Kikinomini/Programmentwurf.git/Programmentwurf/src/de/dhbw/it2016/pe/CSVColumnManager.java
@@ -3,16 +3,17 @@ package de.dhbw.it2016.pe;
 // This class is reserved for further purposes.
 // TODO: Delete magic numbers
 public final class CSVColumnManager {
-	
-    	public final byte STUDENT_ID = 0;
-    	public final byte FIRST_NAME = 1;
-	public final byte LAST_NAME = 2;
-	public final byte STREET = 3;
-	public final byte STREET_NUMBER = 4;
-	public final byte CITY = 5;
-	public final byte ZIP_CODE = 6;
-	public final byte COUNTRY_CODE = 7;
-	public final byte PHONE_AREA_CODE = 8;
-	public final byte PHONE_SUBSCRIBER = 9;
-	public final byte PROVINCE_OPTIONAL = 10;
+    
+    public final byte STUDENT_ID = 0;
+    public final byte FIRST_NAME = 1;
+    public final byte LAST_NAME = 2;
+    public final byte STREET = 3;
+    public final byte STREET_NUMBER = 4;
+    public final byte CITY = 5;
+    public final byte ZIP_CODE = 6;
+    public final byte COUNTRY_CODE = 7;
+    public final byte PHONE_AREA_CODE = 8;
+    public final byte PHONE_SUBSCRIBER = 9;
+    public final byte PROVINCE_OPTIONAL = 10;
+
 }
