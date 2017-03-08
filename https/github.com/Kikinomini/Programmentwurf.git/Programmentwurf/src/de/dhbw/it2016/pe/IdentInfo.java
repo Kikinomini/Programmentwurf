7bab0f9b31211e55d2cfeccbd41dea3ae0040ed9
Iyion <djaros96@yahoo.de>
2017-03-08 18:22:40 +0100

    Added new class - this time for real

---
+++ https/github.com/Kikinomini/Programmentwurf.git/Programmentwurf/src/de/dhbw/it2016/pe/IdentInfo.java
@@ -0,0 +1,14 @@
+package de.dhbw.it2016.pe;
+
+public class IdentInfo {
+
+    public String id;
+    public String firstName;
+    public String lastName;
+
+    public IdentInfo(String id, String firstName, String lastName) {
+	this.id = id;
+	this.firstName = firstName;
+	this.lastName = lastName;
+    }
+}
